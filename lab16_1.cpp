bce4af3ff96ae0748eb0c362ded5d6d7aba32873
Chaichan <chaichanchaisak2549@gmail.com>
2025-01-31 16:22:24 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,50 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr , int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr , int N ,int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = double(rand()%101) / 100 ;
+    }
+}
+
+void findRowSum(const double *dPtr , double *sPtr , int N , int M)
+{
+    double sum = 0;
+    int j = 0;
+    for(int i = 0; i < N*M; i++)
+    {
+        sum += *(dPtr + i);
+        if((i+1)%M == 0)
+        {
+            *(sPtr + j) = sum;
+            sum = 0;
+            j++;
+        }
+    }
+}
+
+void findColSum(const double *dPtr , double *sPtr , int N , int M)
+{
+    double sum = 0;
+    
+    for(int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            sum += *(dPtr + ((j*M)+i));
+        }
+        
+        *(sPtr+i) = sum;
+        sum = 0;
+    }
+}
